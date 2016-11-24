fb0c5510999bf9ab488c2caae435b5aabf2c4580
Dimtion <zizou.xena@gmail.com>
2016-11-24 16:29:22 +0100

    Create minimal development environment

---
+++ vagrant_bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apt-get -qq update
+apt-get -qq upgrade
+
+apt-get -qq install build-essential python-software-properties python3 python3-dev python3-pip
+easy_install3 -U pip  # Solve debian bug
+
+
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+make install
+
+pip3 install -qr /innov/requirements.txt
