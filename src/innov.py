e4e6512feacf9298bd6ac4ded9e1d091c0da197f
Théo Jacquin <42nimag@gmail.com>
2016-11-29 21:45:05 +0100

    Minimal websocket handling

--- src/innov.py
+++ src/innov.py
@@ -32,6 +32,8 @@ def party_manager(room_id):
 
         # Get an action
         action = _a.get()
+        if (action['type'] == 'join'):
+            print (action['uid'])
 
         socketio.emit('Update', {'time': time.time()}, room=_id)
 
@@ -58,22 +60,26 @@ def starting_a_party(message):
 
     global room_by_user
     global parties
+
+    party_to_join = 0
     
-    join_room(parties[0]["name"])
-    room_by_user[uid] = 0
-    parties[0]["actions"].append({"type": "join", "uid": uid})
-
-
+    join_room(parties[party_to_join]["name"])
+    room_by_user[uid] = party_to_join
+    parties[party_to_join]["actions"].put_nowait({"type":"join", "uid":uid})
+    emit( 'party_found', {'parties' : str(parties)})
+    
 @socketio.on('action')
 def player_action(message):
     # Get UID of the user
     uid = session['uid']
 
     global parties
+    
+    parties[room_by_user["uid"]]["actions"].append({"type":message["type"], "uid": uid})
+    
 
-    parties["1"]["actions"].append({"type": message["type"], "uid": uid})
+if __name__ == '__main__':
+    socketio.run(app)
 
-
-party0 = Thread(None, party_manager, None, args=(0,))
+party0 = Thread(target=party_manager, args=(0,) )
 party0.start()
-socketio.run(app)
