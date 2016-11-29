2bae4ebfca1e2069ac8c6fa84c82e00e67fb20a5
Dimtion <zizou.xena@gmail.com>
2016-11-29 21:37:51 +0100

    Move thread launch outside of __main__

--- src/innov.py
+++ src/innov.py
@@ -20,17 +20,16 @@ socketio = SocketIO(app)
 parties = {}
 room_by_user = {}
 
+
 def party_manager(room_id):
     global parties
     parties[room_id] = {"actions" : Queue(), "name":"1"}
     _a = parties[room_id]["actions"]
     _id = room_id
 
-    print ('Pipi caca')
-    
     while True:
         socketio.sleep(1)
-        
+
         # Get an action
         action = _a.get()
 
@@ -38,18 +37,21 @@ def party_manager(room_id):
 
         # Release the lock
         _a.task_done()
-        
+
+
 @app.route('/')
 def hello_world():
     return render_template('client.html')
+
 
 @socketio.on('connect')
 def connect():
     # Choose a UUID
     uid = str(uuid.uuid4())
     # Set it in session var
-    session['uid'] = uid    
-    
+    session['uid'] = uid
+
+
 @socketio.on('looking_for_a_party')
 def starting_a_party(message):
     uid = session['uid']
@@ -59,7 +61,8 @@ def starting_a_party(message):
     
     join_room(parties[0]["name"])
     room_by_user[uid] = 0
-    parties[0]["actions"].append({"type":"join", "uid":uid})
+    parties[0]["actions"].append({"type": "join", "uid": uid})
+
 
 @socketio.on('action')
 def player_action(message):
@@ -67,11 +70,10 @@ def player_action(message):
     uid = session['uid']
 
     global parties
-    
-    parties["1"]["actions"].append({"type":message["type"], "uid": uid})
-    
 
-if __name__ == '__main__':
-    party0 = Thread(target=party_manager, args=(0,) )
-    party0.start()
-    socketio.run(app)
+    parties["1"]["actions"].append({"type": message["type"], "uid": uid})
+
+
+party0 = Thread(None, party_manager, None, args=(0,))
+party0.start()
+socketio.run(app)
